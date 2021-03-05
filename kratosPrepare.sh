a39a05e7207b73207ef0b26efe8901c99e8bfa9d
acupoftea2021 <acupoftea2021@163.com>
2021-03-05 14:39:00 +0800

    go get the google package

--- kratosPrepare.sh
+++ kratosPrepare.sh
@@ -6,8 +6,9 @@ if [ ! -d "$GOPATH" ]; then
 fi
 # download or update the go packages
 echo "\$GOPATH=$GOPATH"
-go get -u -v github.com/golang/protobuf/proto
-go get -u -v github.com/golang/protobuf/protoc-gen-go
+go get -u -v google.golang.org/protobuf/proto
+go get -u -v google.golang.org/protobuf/cmd/protoc-gen-go
+go get -u -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
 go get -u -v github.com/go-kratos/kratos/cmd/kratos/v2@latest
 ls "$GOPATH"/bin
 
@@ -17,6 +18,7 @@ if [ ! -d "$protocDir" ]; then
   mkdir $protocDir
 fi
 cd $protocDir
+
 # download the zip file of protoc
 # to replace the url using the right
 protocDownloadUrl="https://github.com/protocolbuffers/protobuf/releases/download/v3.15.3/protoc-3.15.3-linux-x86_64.zip"
@@ -28,6 +30,7 @@ fi
 unzip "${protocDownloadUrl##*/}"
 sudo cp -rf ./include/* /usr/local/include/
 cp -rf ./bin/protoc "$GOPATH"/bin/
+cp -rf ./include/google "$GOPATH"/src/google
 
 << 'COMMENTS'
 COMMENTS
